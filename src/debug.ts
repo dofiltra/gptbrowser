ca493bdd98f8ac9d6b341b68afc5e25049f7e036
memanot <menemty@gmail.com>
2021-11-13 10:01:18 +0200

    v1.1.30

--- src/debug.ts
+++ src/debug.ts
@@ -1,14 +1,14 @@
-import { extractPageDate } from '.'
-import fetch from 'node-fetch'
-;(async () => {
-  const resp1 = await fetch(
-    'https://noalkogolizm.ru/news/news/2021/11/07/massovo-zaboleli-grippom-igroki-hokkeynogo-kluba-salavat-yulaev.html'
-  )
-  const resp2 = await fetch('https://hotnewstoday24.ru/news-today/mezhdunarodnaya-panorama/12855281')
+// import { extractPageDate } from '.'
+// import fetch from 'node-fetch'
+// ;(async () => {
+//   const resp1 = await fetch(
+//     'https://noalkogolizm.ru/news/news/2021/11/07/massovo-zaboleli-grippom-igroki-hokkeynogo-kluba-salavat-yulaev.html'
+//   )
+//   const resp2 = await fetch('https://hotnewstoday24.ru/news-today/mezhdunarodnaya-panorama/12855281')
 
-  const html1 = await resp1.text()
-  const html2 = await resp2.text()
+//   const html1 = await resp1.text()
+//   const html2 = await resp2.text()
 
-  await extractPageDate(html1)
-  await extractPageDate(html2)
-})()
+//   await extractPageDate(html1)
+//   await extractPageDate(html2)
+// })()
